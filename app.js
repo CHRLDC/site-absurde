9e8ab1755d290cf8cbc7f37d73b9974ad3d7b14d
CharlesWebecom <charles.webecom@gmail.com>
2024-03-28 14:27:57 +0100

    go

---
+++ app.js
@@ -0,0 +1,18 @@
+// Je récupère les éléments du menu burger:
+let burgerMenu = document.getElementById('burger-menu')//le menu
+let fenetreMenu = document.getElementById('fenetre-menu')//la fenêtre
+// J'ajoute un écouteur d'événement pour le clic sur le menu burger:
+burgerMenu.addEventListener('click', function () {
+    // Je bascule la classe "open" lorsque le menu est cliqué:
+    burgerMenu.classList.toggle('open');
+    // Si le menu gurger a la classe open:
+    if (burgerMenu.classList.contains('open')) {
+        // J'ajoute d-block à la fenêtre:
+        fenetreMenu.classList.add('d-block');
+        fenetreMenu.classList.remove('d-none');
+    } else {
+        //sinon j'enlève d-block à la fenêtre:
+        fenetreMenu.classList.remove('d-block');
+        fenetreMenu.classList.add('d-none');
+    }
+});
