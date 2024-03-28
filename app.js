fa8b10b0814898707d887176ef6ec8807138a07b
CharlesWebecom <charles.webecom@gmail.com>
2024-03-28 15:43:23 +0100

    maj

--- app.js
+++ app.js
@@ -16,3 +16,14 @@ burgerMenu.addEventListener('click', function () {
         fenetreMenu.classList.add('d-none');
     }
 });
+
+
+        // JavaScript pour le défilement fluide
+        document.querySelectorAll('.smooth-scroll').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
